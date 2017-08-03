d6eef994d67f85582e6ff0b3212d86cad7a0c6fd
Judzhin Miles <info@msbios.com>
2017-08-03 12:58:40 +0300

    Lazy Composer Commiter

---
+++ src/Exception/InvalidArgumentException.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @access protected
+ * @author Judzhin Miles <info[woof-woof]msbios.com>
+ */
+namespace MSBios\Stdlib\Exception;
+
+use InvalidArgumentException as DefaultInvalidArgumentException;
+
+/**
+ * Class InvalidArgumentException
+ * @package MSBios\Stdlib\Exception
+ */
+class InvalidArgumentException extends DefaultInvalidArgumentException
+{
+
+}
---
+++ src/Object.php
@@ -0,0 +1,512 @@
+<?php
+/**
+ * @access protected
+ * @author Judzhin Miles <info[woof-woof]msbios.com>
+ */
+namespace MSBios\Stdlib;
+
+use MSBios\Stdlib\Exception\InvalidArgumentException;
+use Zend\Json\Json;
+
+/**
+ * Class Object
+ *
+ * Abstract object, all classes are extends from it to automate accessors, generate xml, json or array.
+ *
+ * @package MSBios\Stdlib
+ */
+abstract class Object
+{
+    /**
+     * Original data
+     *
+     * @var array
+     */
+    protected $origData;
+
+    /**
+     * Object attributes
+     *
+     * @var array
+     */
+    protected $data = [];
+
+    /**
+     * Setter/Getter underscore transformation cache
+     *
+     * @var array
+     */
+    protected static $underscoreCache = [];
+
+    /**
+     * @param $id
+     * @return Object
+     */
+    protected function setId($id)
+    {
+        return $this->setData('id', $id);
+    }
+
+    /**
+     * Initialize constructor
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->init();
+    }
+
+    /**
+     *
+     * @return void
+     */
+    public function init()
+    {
+    }
+
+    /**
+     * Add data to the object.
+     *
+     * Retains previous data in the object.
+     *
+     * @param array $arr Data
+     *
+     * @return void
+     */
+    public function addData(array $arr)
+    {
+        foreach ($arr as $index => $value) {
+            $this->setData($index, $value);
+        }
+        return $this;
+    }
+
+    /**
+     * Overwrite data in the object.
+     *
+     * $key can be string or array.
+     * If $key is string, the attribute value will be overwritten by $value
+     *
+     * If $key is an array, it will overwrite all the data in the object.
+     *
+     * @param string|array $key Key
+     * @param mixed $value Value
+     * @return $this
+     */
+    public function setData($key, $value = null)
+    {
+        if (is_array($key)) {
+            $this->data = $key;
+        } else {
+            $this->data[$key] = $value;
+        }
+
+        return $this;
+    }
+
+    /**
+     * Unset data from the object.
+     *
+     * $key can be a string only. Array will be ignored.
+     *
+     * @param null $key
+     * @return $this
+     */
+    public function unsetData($key = null)
+    {
+        if (is_null($key)) {
+            $this->data = [];
+        } else {
+            unset($this->data[$key]);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Retrieves data from the object
+     *
+     * If $key is empty will return all the data as an array
+     * Otherwise it will return value of the attribute specified by $key
+     *
+     * If $index is specified it will assume that attribute data is an array
+     * and retrieve corresponding member.
+     *
+     * @param string $key key
+     * @param string|int $index Index
+     *
+     * @return mixed
+     */
+    public function getData($key = '', $index = null)
+    {
+        if ('' === $key) {
+            return $this->data;
+        }
+        $default = null;
+        // accept a/b/c as ['a']['b']['c']
+        // Not  !== false no need '/a/b always return null
+        if (strpos($key, '/')) {
+            $keyArray = explode('/', $key);
+            $data = $this->data;
+            foreach ($keyArray as $i => $k) {
+                if ($k === '') {
+                    return $default;
+                }
+                if (is_array($data)) {
+                    if (! isset($data[$k])) {
+                        return $default;
+                    }
+                    $data = $data[$k];
+                }
+            }
+            return $data;
+        }
+        // legacy functionality for $index
+        if (isset($this->data[$key])) {
+            if (is_null($index)) {
+                return $this->data[$key];
+            }
+            $value = $this->data[$key];
+            if (is_array($value)) {
+                if (isset($value[$index])) {
+                    return $value[$index];
+                }
+                return null;
+            } elseif (is_string($value)) {
+                $array = explode(PHP_EOL, $value);
+                return (isset($array[$index])
+                    && (! empty($array[$index])
+                        || strlen($array[$index]) > 0)) ? $array[$index] : null;
+            } elseif ($value instanceof Object) {
+                return $value->getData($index);
+            }
+            return $default;
+        }
+        return $default;
+    }
+
+    /**
+     * If $key is empty, checks whether there's any data in the object
+     * Otherwise checks if the specified attribute is set.
+     *
+     * @param string $key Key
+     *
+     * @return boolean
+     */
+    public function hasData($key = '')
+    {
+        if (empty($key) || ! is_string($key)) {
+            return ! empty($this->data);
+        }
+        return array_key_exists($key, $this->data);
+    }
+
+    /**
+     * Convert object attributes to array
+     *
+     * @param array $array array of required attributes
+     *
+     * @return array
+     */
+    public function __toArray(array $array = [])
+    {
+        if (empty($array)) {
+            return $this->data;
+        }
+        /** @var array $arrayResult */
+        $arrayResult = [];
+        foreach ($array as $attribute) {
+            if (isset($this->data[$attribute])) {
+                $arrayResult[$attribute] = $this->data[$attribute];
+            } else {
+                $arrayResult[$attribute] = null;
+            }
+        }
+        return $arrayResult;
+    }
+
+    /**
+     * Public wrapper for __toArray
+     *
+     * @param array $array Data
+     *
+     * @return array
+     */
+    public function toArray(array $array = [])
+    {
+        return $this->__toArray($array);
+    }
+
+    /**
+     * Convert object attributes to XML
+     *
+     * @param array $array Array of required attributes
+     * @param string $rootName Name of the root element
+     * @param boolean $addOpenTag Insert <?xml>
+     * @param boolean $addCdata Insert CDATA[]
+     *
+     * @return string
+     */
+    protected function __toXml(array $array = [], $rootName = 'item', $addOpenTag = false, $addCdata = true)
+    {
+        $xml = '';
+        if ($addOpenTag) {
+            $xml .= '<?xml version="1.0" encoding="UTF-8"?>' . PHP_EOL;
+        }
+        if (empty($array)) {
+            $array = $this->toArray();
+        }
+        if (! empty($rootName) and ! is_numeric($rootName)) {
+            $xml .= '<' . $rootName;
+            if (isset($array['id'])) {
+                $xml .= ' id="' . $array['id'] . '"';
+                unset($array['id']);
+            }
+            $xml .= '>' . PHP_EOL;
+        }
+        foreach ($array as $fieldName => $fieldValue) {
+            if (is_array($fieldValue)) {
+                if (! empty($fieldValue)) {
+                    $xml .= $this->__toXml($fieldValue, $fieldName);
+                    continue;
+                }
+                $fieldValue = '';
+            } elseif (is_object($fieldValue) and method_exists($fieldValue, 'toXml')) {
+                $xml .= $fieldValue->toXml([], $fieldValue->name);
+                continue;
+            }
+            if ($addCdata === true) {
+                $fieldValue = '<![CDATA[' . $fieldValue . ']]>';
+                $xml .= '<' . $fieldName . '>' . $fieldValue . '</' . $fieldName . '>' . PHP_EOL;
+            } else {
+                $fieldValue = htmlentities($fieldValue);
+                $xml .= '<' . $fieldName . '>' . $fieldValue . '</' . $fieldName . '>' . PHP_EOL;
+            }
+        }
+        if (! empty($rootName) and ! is_numeric($rootName)) {
+            $xml .= '</' . $rootName . '>' . PHP_EOL;
+        }
+        return $xml;
+    }
+
+    /**
+     * Public wrapper for __toXml
+     *
+     * @param array $array Data
+     * @param string $rootName Root name
+     * @param boolean $addOpenTag Insert <?xml>
+     * @param boolean $addCdata Insert CDATA[]
+     *
+     * @return string
+     */
+    public function toXml(array $array = [], $rootName = 'item', $addOpenTag = false, $addCdata = true)
+    {
+        return $this->__toXml($array, $rootName, $addOpenTag, $addCdata);
+    }
+
+    /**
+     * Convert object attributes to JSON
+     *
+     * @param array $array array of required attributes
+     *
+     * @return string
+     */
+    protected function __toJson(array $array = [])
+    {
+        return Json::encode($this->toArray($array));
+    }
+
+    /**
+     * Public wrapper for __toJson
+     *
+     * @param array $array Data
+     *
+     * @return string
+     */
+    public function toJson(array $array = [])
+    {
+        return $this->__toJson($array);
+    }
+
+    /**
+     * Public wrapper for __toString
+     *
+     * Will use $format as an template and substitute {{key}} for attributes
+     *
+     * @param string $format Format
+     *
+     * @return string
+     */
+    public function toString($format = '')
+    {
+        if (empty($format)) {
+            $str = implode(', ', $this->getData());
+        } else {
+            preg_match_all('/\{\{([a-z0-9_]+)\}\}/is', $format, $matches);
+            foreach ($matches[1] as $var) {
+                $format = str_replace('{{' . $var . '}}', $this->getData($var), $format);
+            }
+            $str = $format;
+        }
+        return $str;
+    }
+
+    /**
+     * Set/Get attribute wrapper
+     *
+     * @param $method
+     * @param $args
+     * @return bool|mixed|Object
+     */
+    public function __call($method, $args)
+    {
+        switch (substr($method, 0, 3)) {
+            case 'get':
+                $key = $this->underscore(substr($method, 3));
+                $data = $this->getData($key, isset($args[0]) ? $args[0] : null);
+                return $data;
+                break;
+            case 'set':
+                $key = $this->underscore(substr($method, 3));
+                $result = $this->setData($key, isset($args[0]) ? $args[0] : null);
+                return $result;
+                break;
+            case 'uns':
+                $key = $this->underscore(substr($method, 3));
+                $result = $this->unsetData($key);
+                return $result;
+                break;
+            case 'has':
+                $key = $this->underscore(substr($method, 3));
+                return isset($this->data[$key]);
+                break;
+        }
+        throw new InvalidArgumentException('Invalid method ' . get_class($this) . '::' . $method . '(' . print_r($args, 1) . ')');
+    }
+
+    /**
+     * Converts field names for setters and geters
+     *
+     * $this->setMyField($value) === $this->setData('my_field', $value)
+     * Uses cache to eliminate unneccessary preg_replace
+     *
+     * @param string $name Name
+     *
+     * @return string
+     */
+    protected function underscore($name)
+    {
+        if (isset(self::$underscoreCache[$name])) {
+            return self::$underscoreCache[$name];
+        }
+        $result = strtolower(preg_replace('/(.)([A-Z])/', '$1_$2', $name));
+        self::$underscoreCache[$name] = $result;
+        return $result;
+    }
+
+    /**
+     * Implementation of ArrayAccess::offsetSet()
+     *
+     * @param string $offset Offset
+     * @param mixed $value Value
+     *
+     * @return void
+     */
+    public function offsetSet($offset, $value)
+    {
+        $this->data[$offset] = $value;
+    }
+
+    /**
+     * Implementation of ArrayAccess::offsetExists()
+     *
+     * @param string $offset Offset
+     *
+     * @return boolean
+     */
+    public function offsetExists($offset)
+    {
+        return isset($this->data[$offset]);
+    }
+
+    /**
+     * Implementation of ArrayAccess::offsetUnset()
+     *
+     * @param string $offset Offset
+     *
+     * @return void
+     */
+    public function offsetUnset($offset)
+    {
+        unset($this->data[$offset]);
+    }
+
+    /**
+     * Implementation of ArrayAccess::offsetGet()
+     *
+     * @param string $offset Offset
+     *
+     * @return mixed
+     */
+    public function offsetGet($offset)
+    {
+        return isset($this->data[$offset]) ? $this->data[$offset] : null;
+    }
+
+    /**
+     * Get Original data
+     *
+     * @param string $key Key
+     *
+     * @return mixed
+     */
+    public function getOrigData($key = null)
+    {
+        if (is_null($key)) {
+            return $this->origData;
+        }
+        return isset($this->origData[$key]) ? $this->origData[$key] : null;
+    }
+
+    /**
+     * Set Original data
+     *
+     * @param null $key
+     * @param null $data
+     * @return $this
+     */
+    public function setOrigData($key = null, $data = null)
+    {
+        if (is_null($key)) {
+            $this->origData = $this->data;
+        } else {
+            $this->origData[$key] = $data;
+        }
+        return $this;
+    }
+
+    /**
+     * Check if data has changed
+     *
+     * @param string $field Field
+     *
+     * @return boolean
+     */
+    public function hasDataChangedFor($field)
+    {
+        $newdata = $this->getData($field);
+        $origdata = $this->getOrigData($field);
+        return $newdata != $origdata;
+    }
+
+    /**
+     * Current Object name
+     *
+     * @return string
+     */
+    public function getClassName()
+    {
+        return get_class($this);
+    }
+}
